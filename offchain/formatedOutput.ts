80d935c3151462a31362f63225a16d7650b40c85
william_doyle <mcbidebilly@gmail.com>
2022-11-10 11:57:48 -0500

    still works

--- offchain/formatedOutput.ts
+++ offchain/formatedOutput.ts
@@ -24,7 +24,7 @@ export default function formatOutput(__data: string[][]): void {
     const longest_length: number = Math.max(...(_data.map((row) => row.length)))
 
     const coloum_widths: number[] = (Array.from({ length: longest_length })).map((_, i) => {
-        const column = (unzipN<string>(_data) as string[][])[i]
+        const column: string[] = (unzipN<string>(_data) as string[][])[i]
         return Math.max(...(column.map((cell) => cell.length)))
     })
 
@@ -50,62 +50,75 @@ export default function formatOutput(__data: string[][]): void {
         output += (`\n`)
     }
 
-    // output = `${box_drawings.light.tl}${box_drawings.light.h.repeat(maxlinelength - 1)}${box_drawings.light.tr}\n${output}`
 
-    const top_line = (() => {
-        const default_line = `${box_drawings.light.tl}${box_drawings.light.h.repeat(maxlinelength - 1)}${box_drawings.light.tr}`
+    const [top_line, bottom_line] = (() => {
 
-        // get the previous line
         const prev_line = output.split(`\n`)[0]
-        // find indexes of the vertical lines
         const indexes = prev_line.split(``).map((char, i) => [char, i]).filter(([char, i]) => char === box_drawings.light.v).map(([char, i]) => i)
-        console.log(indexes)
+
+        const default_top = `${box_drawings.light.tl}${box_drawings.light.h.repeat(maxlinelength - 1)}${box_drawings.light.tr}`
+        const default_bottom = `${box_drawings.light.bl}${box_drawings.light.h.repeat(maxlinelength - 1)}${box_drawings.light.br}`
 
         if (indexes.length < 3)
-            return default_line
+            return [default_top, default_bottom]
 
         // discard first and last elements of the array
         const _indexes = indexes.slice(1, indexes.length - 1)
-        console.log(_indexes)
 
-        // replace the characters at the indexes with the top junction character
-        return default_line.split(``).map((char, i) => {
-            if (_indexes.includes(i))
-                return box_drawings.light.top_junction
-            return char
-        }).join(``)
+        const insert_junctions = (line: string, jchar: string) => {
+            return line.split(``).map((char, i) => {
+                if (_indexes.includes(i))
+                    return jchar
+                return char
+            }).join(``)
+        }
+
+        const top_line = insert_junctions(default_top, box_drawings.light.top_junction)
+        const bottom_line = insert_junctions(default_bottom, box_drawings.light.bottom_junction)
+        return [top_line, bottom_line]
     })()
+
+    // const top_line = (() => {
+    //     const default_line = `${box_drawings.light.tl}${box_drawings.light.h.repeat(maxlinelength - 1)}${box_drawings.light.tr}`
+    //     // const prev_line = output.split(`\n`)[0]
+    //     // find indexes of the vertical lines
+    //     // const indexes = prev_line.split(``).map((char, i) => [char, i]).filter(([char, i]) => char === box_drawings.light.v).map(([char, i]) => i)
+
+    //     if (indexes.length < 3)
+    //         return default_line
+
+    //     // discard first and last elements of the array
+    //     const _indexes = indexes.slice(1, indexes.length - 1)
+
+    //     // replace the characters at the indexes with the top junction character
+    //     return default_line.split(``).map((char, i) => {
+    //         if (_indexes.includes(i))
+    //             return box_drawings.light.top_junction
+    //         return char
+    //     }).join(``)
+    // })()
 
     output = `${top_line}\n${output}`
 
+    // const bottom_line = (() => {
+    //     const default_line = `${box_drawings.light.bl}${box_drawings.light.h.repeat(maxlinelength - 1)}${box_drawings.light.br}`
+    //     // const prev_line = output.split(`\n`)[output.split(`\n`).length - 2]
+    //     // const indexes = prev_line.split(``).map((char, i) => [char, i]).filter(([char, i]) => char === box_drawings.light.v).map(([char, i]) => i)
 
+    //     if (indexes.length < 3)
+    //         return default_line
 
-    // output += `${box_drawings.light.bl}${box_drawings.light.h.repeat(maxlinelength - 1)}${box_drawings.light.br}`
+    //     // discard first and last elements of the array
+    //     const _indexes = indexes.slice(1, indexes.length - 1)
+    //     console.log(_indexes)
 
-    const bottom_line = (() => {
-        const default_line = `${box_drawings.light.bl}${box_drawings.light.h.repeat(maxlinelength - 1)}${box_drawings.light.br}`
-        // get the previous line
-        const prev_line = output.split(`\n`)[output.split(`\n`).length - 2]
-        // find indexes of the vertical lines
-        const indexes = prev_line.split(``).map((char, i) => [char, i]).filter(([char, i]) => char === box_drawings.light.v).map(([char, i]) => i)
-        console.log(indexes)
-
-        if (indexes.length < 3)
-            return default_line
-
-        // discard first and last elements of the array
-        const _indexes = indexes.slice(1, indexes.length - 1)
-        console.log(_indexes)
-
-        // replace the characters at the indexes with the bottom junction character
-        return default_line.split(``).map((char, i) => {
-            if (_indexes.includes(i))
-                return box_drawings.light.bottom_junction
-            return char
-        }).join(``)
-    })()
-
-
+    //     // replace the characters at the indexes with the bottom junction character
+    //     return default_line.split(``).map((char, i) => {
+    //         if (_indexes.includes(i))
+    //             return box_drawings.light.bottom_junction
+    //         return char
+    //     }).join(``)
+    // })()
 
     output += bottom_line
     output += `\n`
